dec07ba7ac72859940aff4c7a1d43ac54c02700e
James Sansbury <james.sansbury@lullabot.com>
2013-10-13 11:24:16 -0400

    Move navigation to navcontroller.

--- docroot/javascripts/app.js
+++ docroot/javascripts/app.js
@@ -13,3 +13,53 @@ var acornAmpsApp = angular.module('acornAmpsApp', [], function($routeProvider) {
       redirectTo: '/home'
     });
   });
+
+function navController($scope, $location) {
+  $scope.template = 'partials/nav.html';
+
+  $scope.isActive = function (viewLocation) {
+    return viewLocation === '#' + $location.path();
+  };
+
+  $scope.links = [
+    {
+      'href': '#/home',
+      'text': 'Home',
+      'icon': 'icon-home',
+      'title': 'Home'
+    },
+    {
+      'href': '#/find-us',
+      'text': 'Find Us',
+      'icon': 'icon-map-marker',
+      'title': 'Directions to Acorn Amps…'
+    },
+    {
+      'href': '#/contact-us',
+      'text': 'Contact Us',
+      'icon': 'icon-comment',
+      'title': 'Give us a shout…'
+    },
+    {
+      'href': 'http://instagram.com/acornamps',
+      'text': 'Instagram',
+      'icon': 'icon-instagram',
+      'title': 'Check us out on Instagram…',
+      'external': true
+    },
+    {
+      'href': 'http://facebook.com/acornamps',
+      'text': 'Facebook',
+      'icon': 'icon-facebook',
+      'title': 'Check us out on Facebook…',
+      'external': true
+    },
+    {
+      'href': 'http://twitter.com/AcornAmps',
+      'text': 'Twitter',
+      'icon': 'icon-twitter',
+      'title': 'Check us out on Twitter…',
+      'external': true
+    }
+  ];
+}
